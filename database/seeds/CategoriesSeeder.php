b6d5a4466945d52f6ffbebd34d6591b882ec7f07
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2020-01-07 07:43:53 -0600

    Mobile issue: they want the categories icons to be in a PNG format. but the categories returns svg. There fore i have added a new database field 'icon_png' to support android platform. I have edited the : - CategoriesController - Category Model - migration - seeds - categories index.blade

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -13,36 +13,54 @@ class CategoriesSeeder extends Seeder
     public function run()
     {
         factory(App\Category::class)
-            ->create(['name' => 'startups',
-                'icon' => '/svg/startups-icon.svg'])
+            ->create([
+                'name' => 'startups',
+                'icon' => '/svg/startups-icon.svg',
+                'icon_png' => '/mobilePng/markers/startups-marker-icon.png'
+            ])
             ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
 
         factory(App\Category::class)
-            ->create(['name' => 'investors',
-                'icon' => '/svg/investors-icon.svg'])
+            ->create([
+                'name' => 'investors',
+                'icon' => '/svg/investors-icon.svg',
+                'icon_png' => '/mobilePng/markers/investors-marker-icon.png'
+            ])
             ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
         factory(App\Category::class)
-            ->create(['name' => 'accelerators',
-                'icon' => '/svg/accelerators-icon.svg'])
+            ->create([
+                'name' => 'accelerators',
+                'icon' => '/svg/accelerators-icon.svg',
+                'icon_png' => '/mobilePng/markers/accelerators-marker-icon.png'
+            ])
             ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
         factory(App\Category::class)
-            ->create(['name' => 'co-workspaces',
-                'icon' => '/svg/co-workspaces-icon.svg'])
+            ->create([
+                'name' => 'co-workspaces',
+                'icon' => '/svg/co-workspaces-icon.svg',
+                'icon_png' => '/mobilePng/markers/co-workspaces-marker-icon.png'
+            ])
             ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
 
         factory(App\Category::class)
-            ->create(['name' => 'labs',
-                'icon' => '/svg/labs-icon.svg'])
+            ->create([
+                'name' => 'labs',
+                'icon' => '/svg/labs-icon.svg',
+                'icon_png' => '/mobilePng/markers/labs-marker-icon.png'
+            ])
             ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
 
         factory(App\Category::class)
-            ->create(['name' => 'research-centers',
-                'icon' => '/svg/research-centers-icon.svg'])
+            ->create([
+                'name' => 'research-centers',
+                'icon' => '/svg/research-centers-icon.svg',
+                'icon_png' => '/mobilePng/markers/research-centers-marker-icon.png'
+            ])
             ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
     }
