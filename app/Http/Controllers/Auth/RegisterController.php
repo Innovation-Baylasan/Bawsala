d1ff16bb168bd54940025ecca19a49505b4b5dec
owiesnama <owiesnama@gmail.com>
2020-01-10 13:32:20 +0200

    rateable entities

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'username' => User::generateUsername($data['name']),
             'password' => Hash::make($data['password']),
+            'api_token' => Str::random(80),
         ]);
     }
 }
