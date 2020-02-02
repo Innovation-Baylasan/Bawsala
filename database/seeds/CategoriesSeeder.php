002c4b884a480d7203bcbb2cff3526b694fad622
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2020-02-02 15:06:14 +0200

    Adding the following features:
    - reviews should come with user name. (Done)
    
    - review limit 4. (Done)
    
    -review count. (Done)
    
    - following status if this person followed this entity or not (Done).
    
    - his rate if he rated the entity (Done)
    
    - average rate (Done)

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -17,8 +17,8 @@ class CategoriesSeeder extends Seeder
                 'name' => 'startups',
                 'icon' => '/svg/startups-icon.svg',
                 'icon_png' => '/mobilePng/markers/startups-marker-icon.png'
-            ])
-            ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
+            ]);
+            // ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
 
         factory(App\Category::class)
@@ -26,24 +26,24 @@ class CategoriesSeeder extends Seeder
                 'name' => 'investors',
                 'icon' => '/svg/investors-icon.svg',
                 'icon_png' => '/mobilePng/markers/investors-marker-icon.png'
-            ])
-            ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
+            ]);
+            // ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
         factory(App\Category::class)
             ->create([
                 'name' => 'accelerators',
                 'icon' => '/svg/accelerators-icon.svg',
                 'icon_png' => '/mobilePng/markers/accelerators-marker-icon.png'
-            ])
-            ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
+            ]);
+            // ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
         factory(App\Category::class)
             ->create([
                 'name' => 'co-workspaces',
                 'icon' => '/svg/co-workspaces-icon.svg',
                 'icon_png' => '/mobilePng/markers/co-workspaces-marker-icon.png'
-            ])
-            ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
+            ]);
+            // ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
 
         factory(App\Category::class)
@@ -51,8 +51,8 @@ class CategoriesSeeder extends Seeder
                 'name' => 'labs',
                 'icon' => '/svg/labs-icon.svg',
                 'icon_png' => '/mobilePng/markers/labs-marker-icon.png'
-            ])
-            ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
+            ]);
+            // ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
 
         factory(App\Category::class)
@@ -60,8 +60,8 @@ class CategoriesSeeder extends Seeder
                 'name' => 'research-centers',
                 'icon' => '/svg/research-centers-icon.svg',
                 'icon_png' => '/mobilePng/markers/research-centers-marker-icon.png'
-            ])
-            ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
+            ]);
+            // ->entities()->saveMany(factory(App\Entity::class, 5)->make(['category_id' => '']));
 
     }
 }
